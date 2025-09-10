67428e9a086d743c0a22eb0d1b6775d30090a884
EsmaNur07 <esmanurgucin00@gmail.com>
2025-09-10 14:21:34 +0300

    second example for black and white image

--- ocr-test.py
+++ ocr-test.py
@@ -2,16 +2,16 @@ import cv2
 import pytesseract
 from PIL import Image, ImageEnhance
 
-img_file = 'data/Turkish_Technic.png'
+img_file = 'data/bozuk_belge.png'
 img = cv2.imread(img_file)
 
 def grayscale(image):
     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-gray_turkishT = grayscale(img)
-cv2.imwrite("temp/gray_turkishT.png", gray_turkishT)
+gray_image = grayscale(img)
+cv2.imwrite("temp/gray_image.png", gray_image)
 
-thresh, im_blackNwhite3 = cv2.threshold(gray_turkishT, 128, 255, cv2.THRESH_BINARY)
-cv2.imwrite("temp/blackNwhite3.jpg", im_blackNwhite3)
+thresh, im_blackNwhite = cv2.threshold(gray_image, 200, 230, cv2.THRESH_BINARY)
+cv2.imwrite("temp/blackNwhite.png", im_blackNwhite)
 
-img = Image.open("temp/blackNwhite3.jpg")
+img = Image.open("temp/blackNwhite.png")
 img.show() 
