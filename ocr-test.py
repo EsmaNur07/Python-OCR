945052f5e7684d23b2eda02e6cd752c774838a12
EsmaNur07 <esmanurgucin00@gmail.com>
2025-09-10 10:56:16 +0300

    Inverting the colors of an image

--- ocr-test.py
+++ ocr-test.py
@@ -5,5 +5,8 @@ from PIL import Image, ImageEnhance
 img_file = 'data/biruni.jpg'
 img = cv2.imread(img_file)
 
-cv2.imshow('image', img)
-cv2.waitKey(0)
+inverted_img = cv2.bitwise_not(img)
+cv2.imwrite("temp/inverted_img.png", inverted_img)
+
+img = Image.open("temp/inverted_img.png")
+img.show()
