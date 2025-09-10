3df7030e650a76344d6e3f6deaf73ddb3210ff6c
EsmaNur07 <esmanurgucin00@gmail.com>
2025-09-10 13:54:55 +0300

    gray img

--- ocr-test.py
+++ ocr-test.py
@@ -2,11 +2,13 @@ import cv2
 import pytesseract
 from PIL import Image, ImageEnhance
 
-img_file = 'data/biruni.jpg'
+img_file = 'data/bozuk_belge.png'
 img = cv2.imread(img_file)
 
-inverted_img = cv2.bitwise_not(img)
-cv2.imwrite("temp/inverted_img.png", inverted_img)
+def grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray_image = grayscale(img)
+cv2.imwrite("temp/gray_image.png", gray_image)
 
-img = Image.open("temp/inverted_img.png")
+img = Image.open("temp/gray_image.png")
 img.show()
